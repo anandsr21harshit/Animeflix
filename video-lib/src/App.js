c4e6b1f5e1005902483afade56f140c766a65014
Harshit Anand <harshitanand97@gmail.com>
2022-04-17 19:19:29 +0530

    Update: New Routes added

--- video-lib/src/App.js
+++ video-lib/src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import NavBar from "./frontend/components/NavBar/NavBar";
 import LandingPage from "./frontend/pages/LandingPage/LandingPage";
 import HomePage from "./frontend/pages/Home/HomePage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      {/* <LandingPage/> */}
-      <HomePage/>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<HomePage />}></Route>
+        <Route path="/" element={<LandingPage />}></Route>
+      </Routes>
     </div>
   );
 }
