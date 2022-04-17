c1e33746eeddfc43301e1d24f7f549624a4b85d8
Harshit Anand <harshitanand97@gmail.com>
2022-04-17 19:17:46 +0530

    Update NavBar.jsx

--- video-lib/src/frontend/components/NavBar/NavBar.jsx
+++ video-lib/src/frontend/components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import "./NavBar.css"
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <header className="header-nav">
       <nav className="nav-bar">
-        <h1 className="nav-brand">
-            <img src="https://res.cloudinary.com/mranand/image/upload/c_thumb,w_200,g_face/v1650020956/mypics/logo_uk6eef.png" alt="AnimeFilx" />
-        </h1>
+        <Link to={"/"}>
+          <h1 className="nav-brand">
+            <img
+              src="https://res.cloudinary.com/mranand/image/upload/c_thumb,w_200,g_face/v1650020956/mypics/logo_uk6eef.png"
+              alt="AnimeFilx"
+            />
+          </h1>
+        </Link>
         <ul className="menus">
           <li className="menu">
             <a href="#">Sign In</a>
