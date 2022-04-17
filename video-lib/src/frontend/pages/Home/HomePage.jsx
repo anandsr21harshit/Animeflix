ada23f4aa078b3cbea1115bfddbd29c4ecd49343
Harshit Anand <harshitanand97@gmail.com>
2022-04-17 19:15:53 +0530

    Update: VideoCard added

--- video-lib/src/frontend/pages/Home/HomePage.jsx
+++ video-lib/src/frontend/pages/Home/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import SideBar from '../../components/SideBar/SideBar'
 import "./HomePage.css"
+import {videos} from "../../../backend/db/videos"
+import VideoCard from '../../components/VideoCard/VideoCard'
 
 function HomePage() {
   return (
       <div className="home-page-container">
           <SideBar/>
-          <section className='home-page-content'></section>
+          <section className='home-page-content'>
+              <div className="video-container">
+                  {videos.map(video => {
+                      return <VideoCard {...video} />
+                  })}
+              </div>
+          </section>
       </div>
   )
 }
