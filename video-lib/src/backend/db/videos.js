793316a85bb873748cbcf49dc066e4875fcde1c0
Harshit Anand <harshitanand97@gmail.com>
2022-04-17 19:19:07 +0530

    Update: data added

--- video-lib/src/backend/db/videos.js
+++ video-lib/src/backend/db/videos.js
@@ -5,17 +5,63 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "6ecfzeL4KKg",
+    title: "Death Note: Episode 1",
+    videoUrl:"https://youtu.be/6ecfzeL4KKg",
+    category:"Thriller"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "FMxaCRLuRlQ",
+    title: "The Beginning: Netflix Trailer",
+    videoUrl: "https://youtu.be/FMxaCRLuRlQ",
+    category:"Thriller"
   },
+  {
+    _id:"bOoYggSG-oY",
+    title: "Naruto: Season 1 Episode 1",
+    videoUrl: "https://youtu.be/bOoYggSG-oY",
+    category:"Action"
+  },
+  {
+    _id: "9e8kR-dhaug",
+    title: "Death Note: Episode 2",
+    videoUrl: "https://youtu.be/9e8kR-dhaug",
+    category:"Thriller"
+  },
+  {
+    _id:"WCh5F0gd5mk",
+    title: "Beyblade: Season 1, Episode 1",
+    videoUrl: "https://youtu.be/WCh5F0gd5mk",
+    category:"Adventure"
+  },
+  {
+    _id:"p8uT9y4cPwY",
+    title: "Demon Slayer - Rengoku vs Akaza",
+    videoUrl: "https://youtu.be/p8uT9y4cPwY",
+    category:"Action"
+  },
+  {
+    _id:"ouCbRsP5N0I",
+    title: "Naruto: Sasuke vs Itachi Uchiha",
+    videoUrl: "https://youtu.be/ouCbRsP5N0I",
+    category:"Action"
+  },
+  {
+    _id:"xDbg3JWW_hw",
+    title: "Naruto vs Pain",
+    videoUrl: "https://youtu.be/xDbg3JWW_hw",
+    category:"Action"
+  },
+  {
+    _id:"UN66i-JWaWc",
+    title: "Demon Slayer Trailer Season 1",
+    videoUrl: "https://youtu.be/UN66i-JWaWc",
+    category: "Action"
+  },
+  {
+    _id: "_qNmE0xQgfI",
+    title: "Dragon Ball Z",
+    videoUrl:"https://youtu.be/_qNmE0xQgfI",
+    category:"Action"
+  }
 ];
