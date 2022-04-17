4bf5d6bfe4aa62a91478a2fda4f7fdcd37078451
Harshit Anand <harshitanand97@gmail.com>
2022-04-17 19:16:25 +0530

    Create VideoCard.jsx

---
+++ video-lib/src/frontend/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import "./VideoCard.css"
+ 
+function VideoCard({
+    _id,
+    title,
+    videoUrl,
+    category
+}) {
+  return (
+    <div className='video-card'>
+       <Link to={"#"} className="no-text-decoration">
+           <div className="video-img-container">
+               <img src={`https://i.ytimg.com/vi/${_id}/hqdefault.jpg`} alt="Video Thumbnail" className='video-thumbnail' />
+           </div>
+           <div className="video-header">
+               <p className='video-title'>{title}</p>
+               <i class="bi bi-three-dots-vertical"></i>
+           </div>
+           <div className="video-footer">
+               <p className="video-category">{category}</p>
+           </div>
+        </Link> 
+    </div>
+  )
+}
+
+export default VideoCard
