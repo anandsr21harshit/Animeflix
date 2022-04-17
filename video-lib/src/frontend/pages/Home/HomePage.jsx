4e30bdd2dc4c3a3c8ddea465b93c7ff04d137c8f
Harshit Anand <harshitanand97@gmail.com>
2022-04-17 21:31:54 +0530

    Update HomePage.jsx

--- video-lib/src/frontend/pages/Home/HomePage.jsx
+++ video-lib/src/frontend/pages/Home/HomePage.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import SideBar from '../../components/SideBar/SideBar'
 import "./HomePage.css"
-import {videos} from "../../../backend/db/videos"
 import VideoCard from '../../components/VideoCard/VideoCard'
+import { useData } from '../../context/data-context'
 
 function HomePage() {
+    const {state} = useData();
+
   return (
       <div className="home-page-container">
           <SideBar/>
           <section className='home-page-content'>
               <div className="video-container">
-                  {videos.map(video => {
-                      return <VideoCard {...video} />
+                  {state.videos.map(video => {
+                      return <VideoCard {...video} key={video._id} />
                   })}
               </div>
           </section>
