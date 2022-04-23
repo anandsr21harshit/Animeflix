98844c5fc30333669bf0ae35aeb22e175a5a4a37
Harshit Anand <harshitanand97@gmail.com>
2022-04-23 22:46:29 +0530

    Create Login.jsx

---
+++ src/frontend/pages/Login/Login.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/auth-context";
+import "./Login.css";
+
+function Login() {
+
+    const {loginHandler, token} = useAuth();
+    const navigate = useNavigate();
+
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  function testCredHandler (){
+      setUserDetails({
+          email: "adarshbalika@gmail.com",
+          password: "adarshBalika123"
+      })
+  }
+
+  function formHandler(e){
+      e.preventDefault();
+        loginHandler(userDetails.email,userDetails.password);
+  }
+
+  useEffect(()=>{
+    if(token){
+        setTimeout(()=>{
+            navigate("/home")
+        },1000)
+    }
+  },[token])
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={formHandler}>
+        <h1>Login</h1>
+        <div className="login-item">
+          <label>Email</label>
+          <input
+            type="text"
+            value={userDetails.email}
+            placeholder="johndoe@gmail.com"
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+            required
+          />
+        </div>
+        <div className="login-item">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="password"
+            value={userDetails.password}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, password: e.target.value })
+            }
+            required
+          />
+        </div>
+        <div className="form-footer">
+          <button type="submit" className="btn btn-primary login-btn">Login</button>
+          <button className="btn btn-primary test-btn" onClick={testCredHandler}>
+            Use Test Credential
+          </button>
+        </div>
+        <div className="form-footer">
+          <p>
+            Don't have an account? <Link to={"/signup"}>Sign Up</Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export {Login};
