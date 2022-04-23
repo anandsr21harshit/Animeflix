fa9bb872d7d7ac548f5c121747ff040fe8ec9f9f
Harshit Anand <harshitanand97@gmail.com>
2022-04-23 23:13:26 +0530

    New routes added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
 import NavBar from "./frontend/components/NavBar/NavBar";
-import LandingPage from "./frontend/pages/LandingPage/LandingPage";
-import HomePage from "./frontend/pages/Home/HomePage";
-import Stream from "./frontend/pages/Stream/Stream";
+import {
+  LandingPage,
+  HomePage,
+  Stream,
+  Login,
+  History,
+  Playlist,
+  Like,
+  Watchlater,
+} from "./frontend/pages";
 import { Route, Routes } from "react-router-dom";
+import { useAuth } from "./frontend/context/auth-context";
 
 function App() {
+  const { token } = useAuth();
+
   return (
     <div className="App">
       <NavBar />
       <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/playlist" element={<Playlist />}></Route>
+        <Route path="/history" element={<History />}></Route>
+        <Route path="/liked" element={<Like />}></Route>
+        <Route path="/watchlater" element={<Watchlater />}></Route>
         <Route path="/home" element={<HomePage />}></Route>
         <Route path="/home/:videoID" element={<Stream />}></Route>
         <Route path="/" element={<LandingPage />}></Route>
