1e18e70210ca3dbce007642f7f1ad28dd4c86270
Harshit Anand <harshitanand97@gmail.com>
2022-04-25 23:21:22 +0530

    Update Like.jsx

--- src/frontend/pages/Liked/Like.jsx
+++ src/frontend/pages/Liked/Like.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import SideBar from "../../components/SideBar/SideBar";
+import VideoCard from "../../components/VideoCard/VideoCard";
+import { useData } from "../../context/data-context";
+import "./Like.css";
 
 function Like() {
+  const { state } = useData();
+  const { likedVideos } = state;
+
   return (
-    <div>Like</div>
-  )
+    <div className="like-page-container">
+      <SideBar></SideBar>
+      <div className="video-wrapper">
+        {likedVideos.length == 0 && <h1>Playlist is empty</h1>}
+        {likedVideos.length > 0 &&
+          likedVideos.map((video) => {
+            return <VideoCard {...video} key={video._id} />;
+          })}
+      </div>
+    </div>
+  );
 }
 
-export {Like}
+export { Like };
