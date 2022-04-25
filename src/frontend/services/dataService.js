72174b166595a5d07384215dc45f4fa0af30eb3b
Harshit Anand <harshitanand97@gmail.com>
2022-04-25 23:24:22 +0530

    Update dataService.js

--- src/frontend/services/dataService.js
+++ src/frontend/services/dataService.js
@@ -1,25 +1,35 @@
-import axios from "axios"
+import axios from "axios";
 
-async function getVideos(){
-    try{
-        const response = await axios.get("/api/videos");
+async function getVideos() {
+  try {
+    const response = await axios.get("/api/videos");
 
-        if(response.status === 200) return response.data
-    }
-    catch(error){
-        console.error(error.response)
-    }
+    if (response.status === 200) return response.data;
+  } catch (error) {
+    console.error(error.response);
+  }
 }
 
-async function getCategories(){
-    try{
-        const response = await axios.get("/api/categories");
+async function getCategories() {
+  try {
+    const response = await axios.get("/api/categories");
 
-        if(response.status === 200) return response.data;
-    }
-    catch(error){
-        console.error(error.response);
-    }
+    if (response.status === 200) return response.data;
+  } catch (error) {
+    console.error(error.response);
+  }
 }
 
-export {getVideos,getCategories}
+// async function addToLikedVideos(encodedToken, video) {
+//   return axios.post(
+//     "/api/user/likes",
+//     { video },
+//     {
+//       headers: {
+//         authorization: encodedToken,
+//       },
+//     }
+//   );
+// }
+
+export { getVideos, getCategories };
