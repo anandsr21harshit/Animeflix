bf06205ab3c457dd04efc540ae584c80fca3ae28
Harshit Anand <harshitanand97@gmail.com>
2022-04-25 23:24:05 +0530

    like handler added

--- src/frontend/pages/Stream/Stream.jsx
+++ src/frontend/pages/Stream/Stream.jsx
@@ -2,20 +2,39 @@ import React from "react";
 import SideBar from "../../components/SideBar/SideBar";
 import ReactPlayer from "react-player";
 import "./Stream.css";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useData } from "../../context/data-context";
+import { useAuth } from "../../context/auth-context.js";
 
 function Stream() {
   const { videoID } = useParams();
-  const {state} = useData();
-  const {videos} = state;
+  const navigate = useNavigate();
+  const { state, addToLikedVideos, deleteFromLikedVideos } = useData();
+  const { videos } = state;
+  const { token } = useAuth();
 
-  function findVideo(id){
-      const currentVideo = videos.find(video => video._id === id)
-      return currentVideo;
+  function findVideo(id) {
+    const currentVideo = videos.find((video) => video._id === id);
+    return currentVideo;
   }
 
   const playingVideo = findVideo(videoID);
+
+  function likeHandler() {
+    if (token) {
+      if (state.likedVideos.some((video) => video._id === videoID)) {
+        console.log("disliked");
+        deleteFromLikedVideos(playingVideo);
+      } else {
+        console.log("liked");
+        addToLikedVideos(playingVideo);
+      }
+    } else {
+      navigate("/login");
+    }
+  }
+
+  console.log(state.likedVideos);
 
   return (
     <main className="stream-container">
@@ -29,12 +48,32 @@ function Stream() {
               width="100%"
             ></ReactPlayer>
             <div className="video-description">
-            {/* using option chaining so that it will not throw error while re-rendering until data is fetched */}
-                <h3>{playingVideo?.title}</h3> 
-                <div className="video-options">
-                    <span><i className="bi bi-hand-thumbs-up-fill"></i>Like</span>
-                    <span><i className="bi bi-music-note-list"></i>Add to Playlist</span>
-                </div>
+              {/* using option chaining so that it will not throw error while re-rendering until data is fetched */}
+              <h3>{playingVideo?.title}</h3>
+              <div className="video-options">
+                <span
+                  onClick={likeHandler}
+                  className={
+                    state.likedVideos.some((video) => video._id === videoID)
+                      ? "fill-cta"
+                      : ""
+                  }
+                >
+                  <i
+                    className={
+                      state.likedVideos.some((video) => video._id === videoID)
+                        ? "bi bi-hand-thumbs-up-fill fill-cta"
+                        : "bi bi-hand-thumbs-up-fill"
+                    }
+                  ></i>
+                  {state.likedVideos.some((video) => video._id === videoID)
+                    ? "Liked"
+                    : "Like"}
+                </span>
+                <span>
+                  <i className="bi bi-music-note-list"></i>Add to Playlist
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -43,4 +82,4 @@ function Stream() {
   );
 }
 
-export {Stream};
+export { Stream };
