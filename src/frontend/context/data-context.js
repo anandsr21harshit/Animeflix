8c297291b4fb1eab3d28a5b4ac1552073673818d
Harshit Anand <harshitanand97@gmail.com>
2022-05-04 00:59:02 +0530

    Update: delete from history functions added

--- src/frontend/context/data-context.js
+++ src/frontend/context/data-context.js
@@ -8,69 +8,142 @@ const DataContext = createContext();
 
 const initialState = {
   videos: [],
-  categories:[],
-  category: "All",    // for filtering
-  likedVideos: []
+  categories: [],
+  category: "All", // for filtering
+  likedVideos: [],
+  history: [],
 };
 
 const DataProvider = ({ children }) => {
   const [state, dispatch] = useReducer(dataReducer, initialState);
-  const {token} = useAuth()
+  const { token } = useAuth();
 
-  useEffect(()=> {
-    async function fetchData () {
-        const videoResponse = await getVideos();
-        dispatch({ type: "SET_VIDEOS", payload: videoResponse.videos });
-        
-        const categoryResponse = await getCategories();
-        dispatch({type: "SET_CATEGORIES", payload: categoryResponse.categories});
+  useEffect(() => {
+    async function fetchData() {
+      const videoResponse = await getVideos();
+      dispatch({ type: "SET_VIDEOS", payload: videoResponse.videos });
+
+      const categoryResponse = await getCategories();
+      dispatch({
+        type: "SET_CATEGORIES",
+        payload: categoryResponse.categories,
+      });
     }
     fetchData();
-  },[])
+  }, []);
 
-  async function addToLikedVideos(video){
-    try{
-      const likedResponse = await axios.post("/api/user/likes",
+  async function addToLikedVideos(video) {
+    try {
+      const likedResponse = await axios.post(
+        "/api/user/likes",
         {
-          video
+          video,
         },
         {
           headers: {
-            authorization: token
-          }
+            authorization: token,
+          },
         }
-      )
+      );
       console.log(likedResponse);
-      if(likedResponse.status === 201){
-        dispatch({type: "SET_LIKED_VIDEOS", payload: likedResponse.data.likes})
+      if (likedResponse.status === 201) {
+        dispatch({
+          type: "SET_LIKED_VIDEOS",
+          payload: likedResponse.data.likes,
+        });
       }
-    }
-    catch(error){
+    } catch (error) {
       console.error(error.response);
     }
   }
 
-  async function deleteFromLikedVideos(video){
-    try{
-      const response = await axios.delete(`/api/user/likes/${video._id}`,
-      {
-        headers:{
-          authorization: token
-        }
+  async function deleteFromLikedVideos(video) {
+    try {
+      const response = await axios.delete(`/api/user/likes/${video._id}`, {
+        headers: {
+          authorization: token,
+        },
       });
 
-      console.log(response);
-      if(response.status === 200){
-        dispatch({type: "SET_LIKED_VIDEOS", payload: response.data.likes})
+      if (response.status === 200) {
+        dispatch({ type: "SET_LIKED_VIDEOS", payload: response.data.likes });
       }
+    } catch (error) {
+      console.error(error.response);
     }
-    catch(error){
-      console.error(error.response)
+  }
+
+  async function addToHistory(video) {
+    try {
+      const historyResponse = await axios.post(
+        "/api/user/history",
+        { video },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+
+      if (historyResponse.status === 201) {
+        dispatch({
+          type: "SET_HISTORY",
+          payload: historyResponse.data.history,
+        });
+      }
+    } catch (error) {
+      console.error(error.response);
+    }
+  }
+
+  async function deleteFromHistory(videoID) {
+    try {
+      const response = await axios.delete(`/api/user/history/${videoID}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      if(response.status === 200){
+        dispatch({
+          type: "SET_HISTORY",
+          payload: response.data.history,
+        })
+      }
+    } catch (error) {
+      console.error(error.response);
+    }
+  }
+
+  async function deleteAllFromHistory(videoID){
+    try {
+      const response = await axios.delete(`/api/user/history/all`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      if(response.status === 200){
+        dispatch({
+          type: "SET_HISTORY",
+          payload: response.data.history,
+        })
+      }
+    } catch (error) {
+      console.error(error.response);
     }
   }
 
   return (
-    <DataContext.Provider value={{ state, dispatch,addToLikedVideos, deleteFromLikedVideos }}>
+    <DataContext.Provider
+      value={{
+        state,
+        dispatch,
+        addToLikedVideos,
+        deleteFromLikedVideos,
+        addToHistory,
+        deleteFromHistory,
+        deleteAllFromHistory
+      }}
+    >
       {children}
     </DataContext.Provider>
   );
