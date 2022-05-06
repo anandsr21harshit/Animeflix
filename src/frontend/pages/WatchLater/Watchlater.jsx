a27f37f77645ef5a50900fcdef566f251d96eada
Harshit Anand <harshitanand97@gmail.com>
2022-05-06 17:48:14 +0530

    Update Watchlater.jsx

--- src/frontend/pages/WatchLater/Watchlater.jsx
+++ src/frontend/pages/WatchLater/Watchlater.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import SideBar from '../../components/SideBar/SideBar'
+import VideoCard from '../../components/VideoCard/VideoCard';
+import { useData } from '../../context/data-context'
+import "./watchlater.css"
 
 function Watchlater() {
+  const {state} = useData();
+  const {watchlater} = state;
+
   return (
-    <div>Watchlater</div>
+    <div className="watchlater-page-container">
+      <SideBar/>
+      <div className="video-wrapper">
+       {watchlater.length === 0 && <h1>No videos added to watch later yet</h1>}
+       {watchlater.length > 0 && 
+          watchlater.map(watched => <VideoCard {...watched} key={watched._id}/>)}
+      </div>
+    </div>
   )
 }
 
