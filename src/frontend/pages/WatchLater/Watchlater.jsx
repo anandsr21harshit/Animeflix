68b24bb1f59c56f7deae96a60e1f24365cfcecae
Harshit Anand <harshitanand97@gmail.com>
2022-05-11 11:54:59 +0530

    Added sematic HTML

--- src/frontend/pages/WatchLater/Watchlater.jsx
+++ src/frontend/pages/WatchLater/Watchlater.jsx
@@ -11,11 +11,11 @@ function Watchlater() {
   return (
     <div className="watchlater-page-container">
       <SideBar/>
-      <div className="video-wrapper">
+      <section className="video-wrapper">
        {watchlater.length === 0 && <h1>No videos added to watch later yet</h1>}
        {watchlater.length > 0 && 
           watchlater.map(watched => <VideoCard {...watched} key={watched._id}/>)}
-      </div>
+      </section>
     </div>
   )
 }
