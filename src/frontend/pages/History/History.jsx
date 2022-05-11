c50ae9137a8f2e9d6cd1d255e436865044361785
Harshit Anand <harshitanand97@gmail.com>
2022-05-11 11:55:19 +0530

    Added sematic HTML

--- src/frontend/pages/History/History.jsx
+++ src/frontend/pages/History/History.jsx
@@ -15,13 +15,13 @@ function History() {
   return (
     <div className='history-page-container'>
       <SideBar/>
-      <div className="history-video-wrapper">
+      <section className="history-video-wrapper">
         {historyVideo.length === 0 && <h1>You haven't watched any video yet</h1>}
         {historyVideo.length > 0 && <div><button className='btn btn-primary history-btn' onClick={()=>clearHandler(historyVideo._id)}>Clear History</button></div>}
         {historyVideo.length > 0 && historyVideo.map(video => {
           return <VideoCard {...video} key={video._id} />
         })}
-      </div>
+      </section>
     </div>
   )
 }
